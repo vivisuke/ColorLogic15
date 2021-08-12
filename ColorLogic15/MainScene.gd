71b165e0f6a8bfefe8a0475807289e18ed759e86
vivisuke <ntsuda@master.email.ne.jp>
2021-08-12 10:54:49 +0900

    0012 ◎ C 08/12 08/12 0.001│ToDo│盤面クリック処理

--- ColorLogic15/MainScene.gd
+++ ColorLogic15/MainScene.gd
@@ -1,9 +1,32 @@
 extends Node2D
 
+onready var g = get_node("/root/Global")
+
 var crnt_color = 3
 
 func _ready():
+	for y in range(g.N_IMG_CELL_VERT):
+		for x in range(g.N_IMG_CELL_HORZ):
+			$BoardBG/TileMap.set_cell(x, y, 0)
 	pass # Replace with function body.
+func posToXY(pos):
+	var xy = $BoardBG/TileMap.world_to_map(pos - $BoardBG/TileMap.global_position)
+	if xy.x < 0 || xy.x >= g.N_IMG_CELL_HORZ || xy.y < 0 || xy.y >= g.N_IMG_CELL_VERT:
+		return Vector2(-1, -1)
+	return xy
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		var xy = posToXY(event.position)
+		print(xy)
+		if xy.x >= 0:
+			cell_pressed(xy.x, xy.y)
+func cell_pressed(x, y):
+	#print("(", x, ", ", y, ")")
+	if $BoardBG/TileMap.get_cell(x, y) == crnt_color:
+		$BoardBG/TileMap.set_cell(x, y, 0)
+	else:
+		$BoardBG/TileMap.set_cell(x, y, crnt_color)
+	pass
 
 
 func _on_Color3Button_pressed():
